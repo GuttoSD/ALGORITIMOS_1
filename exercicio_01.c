4802ee6809602067fc19853b696cd5ca179e559c
Gutto Silva Dourado <gutto.dourado@estudante.ifms.edu.br>
2025-04-24 19:16:08 -0300

    exercicio_01.c

--- exercicio_01.c
+++ exercicio_01.c
@@ -1,27 +1,31 @@
-//Exercício 1: identificar se é um triângulo, e qual seu tipo
+/* Esse código recebe três valores digitados pelo usuário e verifica se eles podem formar um triângulo. 
+Se sim, ele identifica qual tipo de triângulo é: equilátero (todos os lados iguais), isósceles (dois lados iguais) ou escaleno (todos diferentes). 
+Caso os valores não obedeçam à condição necessária para formar um triângulo, ele avisa que não se trata de um triângulo.
+É um código simples e direto pra entender a lógica básica dos tipos de triângulos.*
+
 #include <stdio.h>
 
    int main (void){
    
-    float a,b,c;
+    float lado_a,lado_b,lado_c;
 
-    printf("Digite os 3 valores do triângulo:\n");
+    printf("Digite os valores a,b e c de um triângulo:\n");
     
-    scanf("%f %f %f",&a, &b, &c);
+    scanf("%f %f %f",&lado_a, &lado_b, &lado_c);
     
-    if (b+c>a && a+c>b && a+b>c){
-     if (a == b && b == c){
+    if (lado_b+lado_c>lado_a && lado_a+lado_c>lado_b && lado_a+lado_b>lado_c){
+     if (lado_a == lado_b && lado_b == lado_c){
         printf(" É um triângulo equilátero \n");
      }
 
      else 
      
-     if (a == b || a == c || b == c){
+     if (lado_a == lado_b || lado_a == lado_c || lado_b == lado_c){
         printf("É um triângulo isóceles \n");
      }
      
      else{
-        printf("É um trinângulo escaleno \n");
+        printf("É um triângulo escaleno \n");
      }
      
     }
