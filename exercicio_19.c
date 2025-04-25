3a9dce8dba04925f4981233a692b18fac1d90a7d
Gutto Silva Dourado <gutto.dourado@estudante.ifms.edu.br>
2025-04-24 22:13:26 -0300

    exercicio_19.c

--- exercicio_19.c
+++ exercicio_19.c
@@ -1,12 +1,14 @@
-// Exercício 19: Multiplicar dois números reais e garantir que o resultado seja inteiro
+/* Esse código pede dois números reais ao usuário e os multiplica.  
+O resultado da multiplicação é convertido para inteiro usando a conversão explícita "int". */
+
 #include <stdio.h>
 
    int main() {
-    float a, b;
+    float num_1, num_2;
 
     printf("Digite dois numeros reais: ");
-    scanf("%f %f", &a, &b);
+    scanf("%f %f", &num_1, &num_2);
 
-    int resultado = (int)(a * b); // Converter o resultado para inteiro
+    int resultado = (int)(num_1 * num_2); // Converter o resultado para inteiro
     printf("O resultado da multiplicacao, convertido para inteiro, é: %d\n", resultado);
 }
