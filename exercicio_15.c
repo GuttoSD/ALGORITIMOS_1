a27fa22f4a7a9b7d30f41110225f85bf9ebb7cc5
Gutto Silva Dourado <gutto.dourado@estudante.ifms.edu.br>
2025-04-24 22:08:55 -0300

    exercicio_15.c

--- exercicio_15.c
+++ exercicio_15.c
@@ -1,13 +1,16 @@
-// Exercício 15: Somar três números inteiros e calcular a média
+/* Esse código pede três números inteiros ao usuário, soma esses números e calcula a média.  
+A soma é feita e armazenada em `soma`, e a média é calculada dividindo a soma por 3.0 (para garantir a precisão decimal).  
+Os resultados da soma e da média são exibidos na tela. */
+
 #include <stdio.h>
 
    int main() {
-    int a, b, c;
+    int num_a, num_b, num_c;
 
     printf("Digite três numeros inteiros: ");
-    scanf("%d %d %d", &a, &b, &c);
+    scanf("%d %d %d", &num_a, &num_b, &num_c);
 
-    int soma = a + b + c;
+    int soma = num_a + num_b + num_c;
     float media = soma / 3.0;
 
     printf("A soma dos numeros é: %d\n", soma);
