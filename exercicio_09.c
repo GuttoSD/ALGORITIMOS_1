778001b5e57bab299e8e343ef367e83cc64e2868
Gutto Silva Dourado <gutto.dourado@estudante.ifms.edu.br>
2025-04-24 21:39:33 -0300

    exercicio_09.c

--- exercicio_09.c
+++ exercicio_09.c
@@ -1,4 +1,4 @@
-/* Exercício 9: Esse código pede um número inteiro ao usuário e verifica se ele é par ou ímpar.  
+/* Esse código pede um número inteiro ao usuário e verifica se ele é par ou ímpar.  
 Usa o operador de módulo (%) para fazer essa verificação e exibe a resposta na tela.  
 É literalmente uma forma "fácil" de trabalhar com condicionais e divisões. */
 
