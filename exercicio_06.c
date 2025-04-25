c9b0ed33295c445e46c0746381ef0e422397ede9
Gutto Silva Dourado <gutto.dourado@estudante.ifms.edu.br>
2025-04-24 21:30:36 -0300

    exercicio_06.c

--- exercicio_06.c
+++ exercicio_06.c
@@ -1,11 +1,13 @@
-// Exercício 6: Somar dois números inteiros
+/* Esse código pede para o usuário digitar dois números inteiros. 
+Depois, ele soma esses dois números e mostra o resultado na tela. */
+
 #include <stdio.h>
 
    int main() {
-    int a, b;
+    int num_a, num_b;
 
     printf("Digite dois numeros inteiros: ");
-    scanf("%d %d", &a, &b);
+    scanf("%d %d", &num_a, &num_b);
 
-    printf("A soma dos numeros é: %d\n", a + b);
+    printf("A soma dos numeros é: %d\n", num_a + num_b);
 }
