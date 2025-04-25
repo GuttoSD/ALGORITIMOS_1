649411257e877efaa4b84f7e42e1dccb1f0c7b76
Gutto Silva Dourado <gutto.dourado@estudante.ifms.edu.br>
2025-04-24 22:23:05 -0300

    exercicio_23.c

--- exercicio_23.c
+++ exercicio_23.c
@@ -1,28 +1,20 @@
-// Exercício 23: Calcular a sequência de Fibonacci utilizando memoização
+/* Calcular a sequência de Fibonacci  
+Solicita um número ao usuário e exibe o n-ésimo termo */ 
+
 #include <stdio.h>
 
-#define MAX 1000
-
-long long fib[MAX];
-
-long long fibonacci(int n) {
+int fibonacci(int n) {
     if (n <= 1) return n;
-    if (fib[n] != -1) return fib[n];
-
-    fib[n] = fibonacci(n - 1) + fibonacci(n - 2);
-    return fib[n];
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-   int main() {
+int main() {
     int n;
 
     printf("Digite um numero para calcular a sequencia de Fibonacci: ");
     scanf("%d", &n);
 
-    // Inicializando o vetor de memoização
-    for (int i = 0; i < MAX; i++) {
-        fib[i] = -1;
-    }
+    int resultado = fibonacci(n);
 
-    printf("O %d-ésimo número de Fibonacci é: %lld\n", n, fibonacci(n));
+    printf("O %d-ésimo número de Fibonacci é: %d\n", n, resultado);
 }
