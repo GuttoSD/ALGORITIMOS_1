548b88cfb3d4f0f49b63a87a63d6e08f71a8a0e3
Gutto Silva Dourado <gutto.dourado@estudante.ifms.edu.br>
2025-04-24 21:33:26 -0300

    exercicio_07.c

--- exercicio_07.c
+++ exercicio_07.c
@@ -1,15 +1,17 @@
-// Exercício 7: Somar dois pares de números e dividir o resultado
+/* Esse código pede ao usuário dois pares de números inteiros. 
+Depois, ele soma o primeiro par e o segundo par separadamente, e divide a soma do primeiro pelo segundo. 
+O resultado da divisão é mostrado com duas casas decimais. */
 #include <stdio.h>
 
    int main() {
-    int a, b, c, d;
+    int num_1, num_2, num_3, num_4;
     float resultado;
 
     printf("Digite dois numeros inteiros: ");
-    scanf("%d %d", &a, &b);
+    scanf("%d %d", &num_1, &num_2);
     printf("Digite mais dois numeros inteiros: ");
-    scanf("%d %d", &c, &d);
+    scanf("%d %d", &num_3, &num_4);
 
-    resultado = (a + b) / (float)(c + d); 
+    resultado = (num_1 + num_2) / (float)(num_3 + num_4); 
     printf("O resultado da divisao é: %.2f\n", resultado);
 }
